97eaea7a7371b32679dbd69cfd9a9671c93484d3
jakubsmiarowski <48868566+jakubsmiarowski@users.noreply.github.com>
2019-11-27 13:52:02 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
+import io from 'socket.io-client';
+
 import './App.css';
+import MessageForm from './components/MessageForm/MessageForm';
+import MessageList from './components/MessageList/MessageList';
+import UsersList from './components/UsersList/UsersList';
+import UserForm from './components/UserForm/UserForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const socket = io('/');
+
+class App extends Component {
+    constructor(props) {
+    super(props);
+    this.state = {users: [], messages: [], text: '', name: ''};
+    };
+    
+    componentDidMount() {
+        socket.on('message', message => this.messageReceive(message));
+        socket.on('update', ({users}) => this.chatUpdate(users));
+    }
+
+    messageReceive(message) {
+        const messages = [message, ...this.state.messages];
+        this.setState({messages});
+    }
+
+    chatUpdate(users) {
+        this.setState({users});
+    }
+
+    handleMessageSubmit(message) {
+        const messages = [message, ...this.state.messages];
+        this.setState({messages});
+        socket.emit('message', message);
+    }
+
+    handleUserSubmit(name) {
+        this.setState({name});
+        socket.emit('join', name);
+    }
+
+    render() {
+        return this.state.name !== '' ? (
+        this.renderLayout()
+        ) : this.renderUserForm()
+    }
+
+    renderLayout() {
+        return (
+            <div className="App">
+                <div className="AppHeader">
+                    <div className="AppTitle">
+                        ChatApp
+                    </div>
+                    <div className="AppRoom">
+                        App room
+                    </div>
+                </div>
+                <div className="AppBody">
+                    <UsersList
+                        users={this.state.users}
+                    />
+                    <div className="MessageWrapper">
+                    <MessageList
+                        messages={this.state.messages}
+                    />
+                    <MessageForm
+                        onMessageSubmit={message => this.handleMessageSubmit(message)}
+                        name={this.state.name}
+                    />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    
+    renderUserForm() {
+        return (<UserForm onUserSubmit={name => this.handleUserSubmit(name)} />)
+    }
 }
-
-export default App;
+export default hot(module)(App);
